03aace660b8c4f06ee8aabc88ccccb8a2686e083
QuickMixKZ <quickmixkz@gmail.com>
2022-07-14 16:14:35 +0500

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -9,12 +9,12 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CandidateStore {
     private static final CandidateStore INST = new CandidateStore();
 
-    private final Map<Integer, Candidate> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private CandidateStore() {
-        posts.put(1, new Candidate(1, "Mikhail"));
-        posts.put(2, new Candidate(2, "Alexandr"));
-        posts.put(3, new Candidate(3, "Lev"));
+        candidates.put(1, new Candidate(1, "Mikhail"));
+        candidates.put(2, new Candidate(2, "Alexandr"));
+        candidates.put(3, new Candidate(3, "Lev"));
     }
 
     public static CandidateStore instOf() {
@@ -22,6 +22,18 @@ public class CandidateStore {
     }
 
     public Collection<Candidate> findAll() {
-        return posts.values();
+        return candidates.values();
+    }
+
+    public void add(Candidate candidate) {
+        candidates.put(candidate.getId(), candidate);
+    }
+
+    public Object findById(int id) {
+        return candidates.get(id);
+    }
+
+    public void update(Candidate candidate) {
+        candidates.put(candidate.getId(), candidate);
     }
 }
