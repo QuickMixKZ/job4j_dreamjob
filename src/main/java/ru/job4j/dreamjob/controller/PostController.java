e1e44af57b85544e0c9fc894dcad63bfe35d981b
QuickMixKZ <quickmixkz@gmail.com>
2022-07-14 15:04:25 +0500

    4. PostController.updatePost. Редактирование вакансии. [#504850]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
 @Controller
-public class PostControl {
+public class PostController {
 
     private final PostStore store = PostStore.instOf();
 
@@ -28,6 +29,18 @@ public class PostControl {
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
         store.add(post);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/formUpdatePost/{postId}")
+    public String formUpdatePost(Model model, @PathVariable("postId") int id) {
+        model.addAttribute("post", store.findById(id));
+        return "updatePost";
+    }
+
+    @PostMapping("/updatePost")
+    public String updatePost(@ModelAttribute Post post) {
+        store.update(post);
         return "redirect:/posts";
     }
 }
