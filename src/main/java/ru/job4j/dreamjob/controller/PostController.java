ef2d85299f23755f0c4a1eb18cd7a9537640f78d
QuickMixKZ <quickmixkz@gmail.com>
2022-07-15 12:45:50 +0500

    1. Слоеная архитектура. [#504851]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.service.PostService;
 import ru.job4j.dreamjob.store.PostStore;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,11 +15,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Controller
 public class PostController {
 
-    private final PostStore store = PostStore.instOf();
+    private final PostService service = PostService.instOf();
 
     @GetMapping("/posts")
     public String posts(Model model) {
-        model.addAttribute("posts", store.findAll());
+        model.addAttribute("posts", service.findAll());
         return "posts";
     }
 
@@ -30,19 +31,19 @@ public class PostController {
 
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
-        store.add(post);
+        service.add(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/formUpdatePost/{postId}")
     public String formUpdatePost(Model model, @PathVariable("postId") int id) {
-        model.addAttribute("post", store.findById(id));
+        model.addAttribute("post", service.findById(id));
         return "updatePost";
     }
 
     @PostMapping("/updatePost")
     public String updatePost(@ModelAttribute Post post) {
-        store.update(post);
+        service.update(post);
         return "redirect:/posts";
     }
 }
