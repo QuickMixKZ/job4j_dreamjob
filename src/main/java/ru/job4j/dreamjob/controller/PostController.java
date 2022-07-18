f5cbf0b551f822b8304d99cb5ebd5a2ebe705854
QuickMixKZ <quickmixkz@gmail.com>
2022-07-18 15:05:58 +0500

    2. Формы. Списки. [#504854 #322701]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,10 +3,8 @@ package ru.job4j.dreamjob.controller;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.dreamjob.model.City;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.PostService;
@@ -38,6 +36,8 @@ public class PostController {
 
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
+        City city = cityService.findById(post.getCity().getId());
+        post.setCity(city);
         postService.add(post);
         return "redirect:/posts";
     }
@@ -51,6 +51,8 @@ public class PostController {
 
     @PostMapping("/updatePost")
     public String updatePost(@ModelAttribute Post post) {
+        City city = cityService.findById(post.getCity().getId());
+        post.setCity(city);
         postService.update(post);
         return "redirect:/posts";
     }
