123277f107442416f4ed4eae7bdc686e82ef985d
QuickMixKZ <quickmixkz@gmail.com>
2022-07-31 12:13:28 +0500

    2. Тестируем PostController [#504867]

---
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -0,0 +1,75 @@
+package ru.job4j.dreamjob.controller;
+
+import org.junit.Test;
+import org.springframework.ui.Model;
+import ru.job4j.dreamjob.model.City;
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.service.CityService;
+import ru.job4j.dreamjob.service.PostService;
+
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class PostControllerTest {
+    @Test
+    public void whenPosts() {
+        List<Post> posts = Arrays.asList(
+                new Post(1, "New post"),
+                new Post(2, "New post")
+        );
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        when(postService.findAll()).thenReturn(posts);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        HttpSession session = mock(HttpSession.class);
+        String page = postController.posts(model, session);
+        verify(model).addAttribute("posts", posts);
+        assertThat(page, is("posts"));
+    }
+
+    @Test
+    public void whenCreatePost() {
+        Post input = new Post(1, "New post");
+        input.setCity(new City(0, "Zero"));
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String page = postController.createPost(input);
+        verify(postService).add(input);
+        assertThat(page, is("redirect:/posts"));
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        Post input = new Post(1, "New post");
+        input.setCity(new City(0, "Zero"));
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String createPage = postController.createPost(input);
+        verify(postService).add(input);
+        assertThat(createPage, is("redirect:/posts"));
+        input.setName("Edited name");
+        input.setCity(new City(0, "Zero"));
+        String updatePage = postController.updatePost(input);
+        verify(postService).update(input);
+        assertThat(updatePage, is("redirect:/posts"));
+    }
+}
